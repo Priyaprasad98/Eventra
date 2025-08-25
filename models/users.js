d5ccf55a4245b7f4170ed304b0b4c24a48e84c53
Priya98 <priyakumari.prasad15@gmail.com>
2025-08-26 00:05:15 +0530

    altered user schema

--- models/users.js
+++ models/users.js
@@ -2,9 +2,28 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true }
+  firstName: { 
+    type: String, 
+    required: true 
+  },
+  lastName: { 
+    type: String, 
+    required: true, 
+    unique: true 
+  },
+  email: { 
+    type: String, 
+    required: true 
+  },
+  password: { 
+    type: String, 
+    required: true 
+  },
+  userType: { 
+    type: String, 
+    required: true,
+    enum: ["Student","Admin"] 
+  }
 });
 
 module.exports = mongoose.model("User", userSchema);
