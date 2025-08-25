07c620e7918866c9416551d303b6666a6a64c957
Priya98 <priyakumari.prasad15@gmail.com>
2025-08-26 00:04:04 +0530

    add controllers for post Signup and post login

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,18 +1,183 @@
+//external module
+const { validationResult, check } = require("express-validator");
+const bcrypt = require("bcryptjs");
+
+//local module
+const User = require("../models/users");
+const { LogTimings } = require("concurrently");
+
 exports.getLogin = (req, res, next) => {
   res.render("auth/login", {
     pageTitle: "Login",
-    currentPage: "Login"
+    currentPage: "Login",
+    isLoggedIn: false,
+    errors: [],
+    oldInput: {},
+    user: {}
   });
 };
 
 exports.getSignup = (req, res, next) => {
   res.render("auth/signUp", {
     pageTitle: "Sign Up",
-    currentPage: "Login"
+    currentPage: "signup",
+    isLoggedIn: false,
+    oldInput: {},
+    user: {}
   });
 };
 
-exports.postLogin = (req, res, next) => {
-  const {email,password} = req.body;
+exports.postSignup = [
+  //First Name Validation
+  check("firstName")
+  .trim()
+  .notEmpty()
+  .withMessage("First name is required")
+  .isLength({min: 2})
+  .withMessage("First name should be atleast 2 characters long")
+  .matches(/^[A-Z\a-z\s]+$/)
+  .withMessage("First name should only contain alphabets"),
+ 
+  //First Name Validation
+  check("lastName")
+  .matches(/^[A-Z\a-z\s]*$/)
+  .withMessage("Last name should only contain alphabets"),
   
+  //Email validation
+  check("email")
+  .isEmail()
+  .withMessage("Please enter a valid email")
+  .normalizeEmail(),
+
+  //Password Validation
+  check("password")
+  .isLength({min: 8})
+  .withMessage("Password must be at least 8 characters long")
+  .matches(/[a-z]/)
+  .withMessage("Password must contain at least one lowercase letter")
+  .matches(/[A-Z]/)
+  .withMessage("Password must contain at least one uppercase letter")
+  .matches(/[0-9]/)
+  .withMessage("Password must contain at least one number")
+  .matches(/[!@#$%^&]/)
+  .withMessage("Password must contain at least one special character")
+  .trim(),
+
+  //confirm password validation
+  check("confirmPassword")
+  .trim()
+  .custom((value, {req}) => { 
+    //the second argument is meta ie. it's a full object containing request and other details. so we use destructuring 
+   /*custom((value, meta) => {
+      const req = meta.req; 
+      ...
+     }) 
+   */
+    if(value !== req.body.password) {
+      throw new Error("Passwords do not match")
+    }
+    return true;
+  }),
+
+  //user Type Validation
+  check("userType")
+  .notEmpty()
+  .withMessage("User Type is required")
+  .isIn(["Student","Admin"])
+  .withMessage("Invalid User Type"),
+
+  //Terms Accepted Validation
+  check("termsAccepted")
+  .notEmpty()
+  .withMessage("You must accept the terms and conditions")
+  .custom((value,{req}) => {
+    if(value !== "on") {
+      throw new Error("Please accept the terms and conditions");
+    }
+    return true;
+  }),
+  
+  //Final handler Middleware
+  (req, res, next) => {
+  console.log(req.body);
+  const { firstName, lastName, email, password, userType } = req.body;
+  const errors = validationResult(req);
+  console.log(errors);
+  if(!errors.isEmpty()) {
+    return res.status(422).render("auth/signup", {
+      currentPage: 'signup',
+      pageTitle: "Sign Up",
+      isLoggedIn: false,
+      errors: errors.array().map(error => error.msg),
+      user: {},
+      oldInput: {
+        firstName,
+        lastName,
+        email,
+        userType
+      }
+    });
+  }
+  bcrypt.hash(password, 12)
+  .then(hashedPassword => {
+    const user = new User({firstName, lastName, email, password: hashedPassword, userType});
+    return user.save();
+  })
+  .then(() => {
+    res.redirect("/login");
+  })
+  .catch((err) => {
+     console.log("error while saving user:", err);
+      return res.status(422).render("auth/signup", {
+        currentPage: "signup",
+        pageTitle: "Sign Up",
+        isLoggedIn: false,
+        errors: [err.message],
+        user: {},
+        oldInput: {
+          firstName,
+          lastName,
+          email,
+          password,
+          userType
+        }
+      });
+    });
+  }
+];
+  
+
+exports.postLogin = async (req, res, next) => {
+  const {email,password} = req.body;
+  const  user = await User.findOne({email});
+  if(!user) {
+    res.render("auth/login",{
+      pageTitle: Login,
+      currentPage: Login,
+      IsLoggedIn: false,
+      errors: ["Invalid Email"],
+      oldInput: { email },
+      user: {}
+    });
+  }
+  const isMatch = await bcrypt.compare(password,user.password);
+  if(!isMatch) {
+    return res.render("auth/login",{
+      pageTitle: Login,
+      currentPage: Login,
+      IsLoggedIn: false,
+      errors: ["Invalid password"],
+      oldInput: { email },
+      user: {}
+    });
+  }
+  req.session.isLoggedIn = true;
+  req.session.user = user;
+  req.session.save(err => {
+    if(err) {
+     console.log("session save error:",err);
+    }
+    res.redirect("/");
+  })
 };
+
