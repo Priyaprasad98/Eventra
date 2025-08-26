8399c52c23dd10ed70d66d7f8ef3899b09adb19b
Priya98 <priyakumari.prasad15@gmail.com>
2025-08-26 21:19:30 +0530

    fix: removeunique constraint from lastName

--- models/users.js
+++ models/users.js
@@ -8,8 +8,7 @@ const userSchema = new mongoose.Schema({
   },
   lastName: { 
     type: String, 
-    required: true, 
-    unique: true 
+    required: true,
   },
   email: { 
     type: String, 
